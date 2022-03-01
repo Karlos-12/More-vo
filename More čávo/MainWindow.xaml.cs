6012a1b9bdadcd161eee4e9c33328e3da42f207d
Karel Cveček <Cvecek.Karel@stud.vassboskovice.cz>
2022-03-01 09:37:59 +0100

    Přidejte soubory projektu.

---
+++ More čávo/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace More_čávo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Čavo Tomsn1 = new Čavo("Tomsn", "Blaohulič", new DateTime(2006, 3, 21), 100, 100, 0);
+        Čavo Marian1 = new Čavo("Marian", "Čavočum", new DateTime(2006, 5, 12), 100, 100, 0);
+        int who = 0;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            write();
+        }
+
+        public void write()
+        {
+            t1.Text = ("Name: " + Tomsn1.name + " " + Tomsn1.surname  + "\nBirth: " + Tomsn1.birth + "\nEnergdzi: " + Tomsn1.enerdzi + "\nLife: " + Tomsn1.life + "\nXP: " + Tomsn1.Xpcka);
+            t2.Text = ("Name: " + Marian1.name + " " + Marian1.surname + "\nBirth: " + Marian1.birth + "\nEnergdzi: " + Marian1.enerdzi + "\nLife: " + Marian1.life + "\nXP: " + Marian1.Xpcka);
+
+        }
+
+        private void sw_Click(object sender, RoutedEventArgs e)
+        {
+            if(who == 0)
+            {
+                who++;
+                t1.BorderBrush = new SolidColorBrush(Color.FromArgb(128, 0, 0, 0));
+                t2.BorderBrush = new SolidColorBrush(Color.FromArgb(128, 128, 0, 0));
+            }
+            else 
+            {
+                who--;
+                t2.BorderBrush = new SolidColorBrush(Color.FromArgb(128, 0, 0, 0));
+                t1.BorderBrush = new SolidColorBrush(Color.FromArgb(128, 128, 0, 0));
+            }
+        }
+    }
+}
---
+++ More čávo/Čavo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace More_čávo
+{
+    class Čavo
+    {
+        public string name { get; }
+        public string surname { get; }
+        public DateTime birth { get; }
+        public int enerdzi { get; set; }
+        public int life { get; set; }
+        public int Xpcka { get; set; }
+        
+
+        public Čavo(string n, string srn, DateTime b, int eng, int lif, int xp)
+        {
+            name = n;
+            surname = srn;
+            birth = b;
+            enerdzi = eng;
+            life = lif;
+            Xpcka = xp;
+        }
+
+
+    }
+}
